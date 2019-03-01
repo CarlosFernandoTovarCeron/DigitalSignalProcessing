dfcb7967734b4267c90a6551156bd5e0aa8f3289
Carlos Tovar <carlos.fernando.tovar@correounivalle.edu.co>
2019-02-28 19:58:26 -0500

    fix var names and add sine-cosine signal example

---
+++ SineCosineSignal.m
@@ -0,0 +1,15 @@
+% Sine and cosine signals
+signal_frecuency=1; %signal frequency in Hz
+signal_frecuencty_rads_per_sec=2*pi*signal_frecuency; %signal frequency in rad/s
+sampling_frecuency_hz=60; %sampling frequency in Hz
+time_interval=1/sampling_frecuency_hz; %time interval between samples;
+signal_duration=3; % duration of the signal
+time=0:time_interval:(signal_duration-time_interval); %time intervals set
+ys=sin(signal_frecuencty_rads_per_sec*time); %signal data set
+plot(time,ys,'r'); hold on; %plots figure
+axis([0 signal_duration -1.5 1.5]);
+xlabel('seconds');
+yc=cos(signal_frecuencty_rads_per_sec*time); %signal data set
+plot(time,yc,'--k'); %plot with points
+axis([0 3 -1.5 1.5]);
+xlabel('seconds'); title('sine (solid and red) and cosine (dashed)');
--- SineSignal.m
+++ SineSignal.m
@@ -1,11 +1,11 @@
 % Sine signal
 signal_frecuency=2; %signal frequency in Hz
-signal_frecuency_rads=2*pi*signal_frecuency; %signal frequency in rad/s
-sampling_frecuency=60; %sampling frequency in Hz
-time_inverval=1/sampling_frecuency; %time interval between samples;
+signal_frecuency_rads_per_sec=2*pi*signal_frecuency; %signal frequency in rad/s
+sampling_frecuency_hz=60; %sampling frequency in Hz
+time_inverval=1/sampling_frecuency_hz; %time interval between samples;
 signal_duration=5; %signal duration in seconds
 time=0:time_inverval:(signal_duration-time_inverval); %time intervals set
-y=sin(signal_frecuency_rads*time); %signal data set
+y=sin(signal_frecuency_rads_per_sec*time); %signal data set
 plot(time,y,'r'); %plots figure in red
 axis([0 seconds_signal -1.5 1.5]);
 xlabel('seconds'); title('sine signal');
